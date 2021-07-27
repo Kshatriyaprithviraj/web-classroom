b124989a9391e1c9d1b5cb8601af04f7ea1609f9
kshatriyaprithviraj <1999prithviraj1999@gmail.com>
2021-07-27 13:01:26 +0530

    updated  auth controller

---
+++ config/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || 'bLuEoNeLoVE',
+    mongouri: process.env.MONGODB_URI || 
+        process.env.MONGO_HOST || 
+        'mongodb://' + (process.env.IP || 'localhost') + ':' +
+        (process.env.MONGODB_PORT || '27017') + '/eclassDB',
+}
+
+export default config;
